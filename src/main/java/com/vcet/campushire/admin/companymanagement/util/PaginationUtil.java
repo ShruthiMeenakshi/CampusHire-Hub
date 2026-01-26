386d6f30da5b7c8351c83535e323abaf36b64eff
Ragul-05 <ragulgopinath2005@gmail.com>
2026-01-26 10:08:25 +0530

    Added APIResponse,pagination classes  for admin company management Utility classes

--- src/main/java/com/vcet/campushire/admin/companymanagement/util/PaginationUtil.java
+++ src/main/java/com/vcet/campushire/admin/companymanagement/util/PaginationUtil.java
@@ -1,4 +1,12 @@
 package com.vcet.campushire.admin.companymanagement.util;
 
 public class PaginationUtil {
+
+    public static int getPage(int page) {
+        return page < 0 ? 0 : page;
+    }
+
+    public static int getSize(int size) {
+        return size <= 0 ? 10 : size;
+    }
 }
