ed45ede26e5b1038512eb1fd2674f419ef86c361
Ragul-05 <ragulgopinath2005@gmail.com>
2026-01-27 14:27:47 +0530

    refactor(repo): Disable hibernate cache to avoid the stale visits data

--- src/main/java/com/vcet/campushire/admin/companymanagement/repository/CompanyVisitRepository.java
+++ src/main/java/com/vcet/campushire/admin/companymanagement/repository/CompanyVisitRepository.java
@@ -2,12 +2,16 @@ package com.vcet.campushire.admin.companymanagement.repository;
 
 import com.vcet.campushire.admin.companymanagement.model.CompanyVisit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface CompanyVisitRepository extends JpaRepository<CompanyVisit, Long> {
 
-    List<CompanyVisit> findByCompanyId(Long companyId);
+    @Query("SELECT v FROM CompanyVisit v WHERE v.company.id = :companyId")
+    List<CompanyVisit> findByCompanyId(@Param("companyId") Long companyId);
+
 
     List<CompanyVisit> findByCompanyIdAndAcademicYear(Long companyId, String academicYear);
 }
